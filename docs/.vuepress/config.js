c2682417afa996c5672cd1a12b8ae8c80aa188d1
haozj <haozj@fenbi.com>
2020-02-08 20:58:26 +0800

    chore: 配置主题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,35 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around',
-    base: '/my-blog/'
+    title: '阿呆的博客',
+    description: '阿呆的博客',
+    base: '/my-blog/',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' }
+      ],
+      sidebar: [
+        {
+          title: '数据结构',
+          path: '/constractor/',
+          collapsable: false, 
+          sidebarDepth: 1,
+          children: [
+            ['/constractor/stack','栈'],
+            ['/constractor/queue','队列']
+          ]
+        },
+        {
+          title: '算法',
+          path: '/algorithm/',
+          collapsable: false, 
+          sidebarDepth: 2,
+          children: [
+            ['/algorithm/bubblesort','冒泡排序']
+          ]
+        }
+      ],
+      smoothScroll: true,
+      lastUpdated: 'Last Updated',
+      repo: 'https://github.com/tian-cai/my-blog',
+      editLinks: true,
+    }
   }
