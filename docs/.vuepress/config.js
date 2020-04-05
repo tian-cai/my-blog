7ee88a122f5cd6e4d0f0172999b7197b945ffe1d
haozj <xdhaozhaojie@163.com>
2020-04-05 22:30:37 +0800

    feat: finish bubblesort.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,42 +1,54 @@
 module.exports = {
-    title: '阿呆的博客',
-    description: '阿呆的博客',
-    base: '/my-blog/',
-    themeConfig: {
-      nav: [
-        { text: 'Home', link: '/' }
-      ],
-      sidebar: [
-        {
-          title: '数据结构',
-          path: '/constractor/',
-          collapsable: false, 
-          sidebarDepth: 2,
-          children: [
-            ['/constractor/stack','栈'],
-            ['/constractor/queue','队列'],
-            ['/constractor/deque','双端队列'],
-            ['/constractor/tree','树'],
-            ['/constractor/minheap','最小堆'],
-            ['/constractor/maxheap','最大堆']
-          ]
-        },
-        {
-          title: '算法',
-          path: '/algorithm/',
-          collapsable: false, 
-          sidebarDepth: 2,
-          children: [
-            ['/algorithm/bubblesort','冒泡排序']
-          ]
-        }
-      ],
-      smoothScroll: true,
-      lastUpdated: 'Last Updated',
-      repo: 'https://github.com/tian-cai/my-blog',
-      editLinks: true,
-      docsDir: 'docs',
-      docsBranch: 'master',
-      displayAllHeaders: true,
+  title: '阿呆的博客',
+  description: '阿呆的博客',
+  base: '/my-blog/',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' }
+    ],
+    sidebar: [
+      {
+        title: '数据结构',
+        path: '/constractor/',
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [
+          ['/constractor/stack', '栈'],
+          ['/constractor/queue', '队列'],
+          ['/constractor/deque', '双端队列'],
+          ['/constractor/tree', '树'],
+          ['/constractor/minheap', '最小堆'],
+          ['/constractor/maxheap', '最大堆']
+        ]
+      },
+      {
+        title: '算法',
+        path: '/algorithm/',
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [
+          ['/algorithm/bubblesort', '冒泡排序']
+        ]
+      }
+    ],
+    smoothScroll: true,
+    lastUpdated: 'Last Updated',
+    repo: 'https://github.com/tian-cai/my-blog',
+    editLinks: true,
+    docsDir: 'docs',
+    docsBranch: 'master',
+    displayAllHeaders: true,
+  },
+  head: [
+    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css' }],
+    ['link', { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" }]
+  ],
+  markdown: {
+    plugins: ['markdown-it-katex'],
+    extendMarkdown: md => {
+      md.set({ html: true })
+      md.use(require('markdown-it-katex'))
     }
   }
+
+}
--- docs/algorithm/bubblesort.md
+++ docs/algorithm/bubblesort.md
@@ -1,1 +1,42 @@
-> 敬请期待
+### 基本思路
+比较所有相邻的两个项，如果第一个比第二个大，则交换他们的位置。元素项向上移动至正确的顺序，
+就好像气泡升至表面一样，因此叫冒泡排序。复杂度是O($n^2$)
+
+### 具体实现
+```js
+const Compare = {
+    LESS_THAN: -1,
+    BIGGER_THAN: 1,
+    EQUAL: 0
+}
+
+function defaultCompare(a, b) {
+    if (a === b) {
+        return 0
+    }
+    return a < b ? -1 : 1
+}
+
+function swap(array, a, b) {
+    const temp = array[a]
+    array[a] = array[b]
+    array[b] = temp
+}
+
+function bubbleSort(array, compareFn = defaultCompare) {
+    const len = array.length
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (compareFn(array[j], array[j + 1]) === Compare.BIGGER_THAN) {
+                swap(array, j, j + 1)
+            }
+        }
+    }
+    return array
+}
+
+```
+
+
+### 参考文章
+- javascript数据结构与算法第三版
